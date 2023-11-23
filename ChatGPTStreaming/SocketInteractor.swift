e5b495709c2cff8e7f99500d09f03f692ff8f93d
Berserk-21 <cryptogekkofr@protonmail.com>
2023-11-23 15:36:02 +0100

    Ajoute les propriétés socketManager et socketDecoder dans le protocol SocketPresenterInterface

--- ChatGPTStreaming/SocketInteractor.swift
+++ ChatGPTStreaming/SocketInteractor.swift
@@ -9,15 +9,17 @@ import Foundation
 
 protocol SocketPresenterInterface: AnyObject {
     var view: ViewInterface? { get set }
+    var socketManager: SocketManager { get set }
+    var socketDecoder: SocketDecoder { get set }
     func onTextFieldReturn(with input: String)
 }
 
 final class SocketPresenter: SocketPresenterInterface {
     
+    var socketManager: SocketManager = SocketManager()
+    var socketDecoder: SocketDecoder = SocketDecoder()
+    
     weak var view: ViewInterface?
-        
-    private let socketManager = SocketManager()
-    private let socketDecoder = SocketDecoder()
     
     func onTextFieldReturn(with input: String) {
         socketManager.sendChatGPTRequest(input: input, onDataReceived: { result in
