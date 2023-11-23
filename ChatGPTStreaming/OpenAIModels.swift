05fe8ed99e72d77c1d06e6eccb727fa4d02915c0
Berserk-21 <cryptogekkofr@protonmail.com>
2023-11-23 12:26:14 +0100

    Supprime les struct non nécessaires et laisse uniquement celles utiles au stream

--- ChatGPTStreaming/OpenAIModels.swift
+++ ChatGPTStreaming/OpenAIModels.swift
@@ -24,30 +24,3 @@ struct StreamingChoice: Decodable {
 struct StreamingDelta: Decodable {
     let content: String?
 }
-
-struct OpenAIJson: Decodable {
-    let id: String
-    let object: String
-    let created: Int
-    let model: String
-    let usage: Usage
-    let choices: [Choice]
-}
-
-struct Usage: Decodable {
-    let prompt_tokens: Int
-    let completion_tokens: Int
-    let total_tokens: Int
-}
-
-struct Choice: Decodable {
-    let message: Message
-    let finish_reason: String
-    let index: Int
-}
-
-struct Message: Decodable {
-    let role: String
-    let content: String
-}
-
